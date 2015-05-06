a56b83e44a29311eddd13d4b948121a03661edd3
Achim Gsell <achim.gsell@psi.ch>
2015-05-06 15:17:53 +0200

    scripts/Bootstrap: - coreutils added, we need a unique base64

--- scripts/Bootstrap/compile_pmodules.sh
+++ scripts/Bootstrap/compile_pmodules.sh
@@ -8,11 +8,12 @@ source "${BOOTSTRAP_DIR}/Pmodules_version.conf"
 unset PMODULES_HOME
 source "/opt/psi/config/environment.bash"
 
-${BOOTSTRAP_DIR}/gettext/build --bootstrap || { echo "compiling 'gettext' failed!"; exit 1; }
-${BOOTSTRAP_DIR}/getopt/build --bootstrap  || { echo "compiling 'getopt' failed!"; exit 1; }
-${BOOTSTRAP_DIR}/dialog/build --bootstrap  || { echo "compiling 'dialog' failed!"; exit 1; }
-${BOOTSTRAP_DIR}/bash/build --bootstrap    || { echo "compiling 'bash' failed!"; exit 1; }
-${BOOTSTRAP_DIR}/Tcl/build --bootstrap     || { echo "compiling 'Tcl' failed!"; exit 1; }
+${BOOTSTRAP_DIR}/coreutils/build --bootstrap || { echo "compiling 'coreutils' failed!"; exit 1; }
+${BOOTSTRAP_DIR}/gettext/build --bootstrap   || { echo "compiling 'gettext' failed!"; exit 1; }
+${BOOTSTRAP_DIR}/getopt/build --bootstrap    || { echo "compiling 'getopt' failed!"; exit 1; }
+${BOOTSTRAP_DIR}/dialog/build --bootstrap    || { echo "compiling 'dialog' failed!"; exit 1; }
+${BOOTSTRAP_DIR}/bash/build --bootstrap      || { echo "compiling 'bash' failed!"; exit 1; }
+${BOOTSTRAP_DIR}/Tcl/build --bootstrap       || { echo "compiling 'Tcl' failed!"; exit 1; }
 
 # we have to remove the init directory - otherwise the next build will fail...
 rm -rf "${PMODULES_HOME}/init"
