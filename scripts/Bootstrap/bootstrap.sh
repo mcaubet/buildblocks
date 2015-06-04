8781da4c537c834b733af58e3dc1666b6c16b34d
Achim Gsell <achim.gsell@psi.ch>
2015-06-04 18:41:37 +0200

    scripts/Bootstrap/bootstrap.sh: bugfix

--- scripts/Bootstrap/bootstrap.sh
+++ scripts/Bootstrap/bootstrap.sh
@@ -2,6 +2,6 @@
 declare -r BASE_DIR=$(cd "$(dirname $0)/../.." && pwd)
 declare -r BOOTSTRAP_DIR="${BASE_DIR}/scripts/Bootstrap"
 
-${BOOTSTRAP_DIR}/build_pmodules.sh
+${BOOTSTRAP_DIR}/compile_pmodules.sh
 ${BOOTSTRAP_DIR}/install_pmodules.sh
 
